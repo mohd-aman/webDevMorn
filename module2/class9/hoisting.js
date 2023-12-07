6287fb74f3c4efab3e686bc1b6eccfea42a0be26
aman-scaler <mohd.aman@scaler.com>
2023-12-07 09:20:32 +0530

    execution context

---
+++ module2/class9/hoisting.js
@@ -0,0 +1,32 @@
+//access to variable/function before it is declared is known as hoisting
+// console.log(a);
+// var a = 10;
+// console.log(a);
+
+
+// sayHi();
+
+
+// function sayHi(){
+//     console.log("Hello")
+// }
+
+
+// sayHello(); //undefined();
+// var sayHello = function(){
+//     console.log("Hello from function expression");
+// }
+
+// sayHello(); //will work because by this tiime sayHello has function store in it
+
+
+// arrowSayHi();
+
+// var arrowSayHi = ()=>{
+//     console.log("arrow say Hi")
+// }
+
+// console.log(a); 
+let a;
+console.log(a);
+
---
+++ module2/class9/varLetConst.js
@@ -0,0 +1,57 @@
+// var a = 10;
+// console.log(a);
+
+// re assigning of variable is allowed
+// a = 20;
+// console.log(a);
+
+// //redeclaration of variable is allowed.
+// var a = 30;
+// console.log(a);
+
+//can access it before declaration
+// console.log(a);
+// var a;
+
+// let a;
+// a = 10;
+// let a = 10;
+// console.log(a);
+
+// a = 20;
+// console.log(a);
+//redeclaration of variable is not allowed
+// let a = 30;
+
+// cannot access a before initialization
+// console.log(a);
+// let a;
+
+//we need to assign value to variable at the time of declaration
+// const a = 20;
+// console.log(a);
+// //re assignment of variable is not allowed.
+// a = 30;
+
+//re declaraton is not allowed
+// const a = 40;
+
+// const a;
+// a = 20;
+// console.log(a);
+
+// var a = 10;
+// let a = 20;
+
+// console.log(b);
+// const b = 20;
+
+// var c = function(){
+//     console.log("hello");
+// }
+
+// var c;
+
+// console.log(c); // undefined or function.
+
+
