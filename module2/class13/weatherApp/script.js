552320b9a962d10518c12c4758585330c6b7a9f3
aman-scaler <mohd.aman@scaler.com>
2023-12-19 08:16:26 +0530

    weatherApp completed

--- module2/class13/weatherApp/script.js
+++ module2/class13/weatherApp/script.js
@@ -2,14 +2,34 @@
 // 1. make request on click of button.
 // 2. make req with the value we have in input
 
-let btn = document.querySelector("button");
+// let btn = document.querySelector("button");
+let form = document.querySelector("form");
 let input = document.querySelector("input");
+let temp = document.querySelector('.temp');
+let place = document.querySelector('.time_location p');
+let timeDate = document.querySelector('.time_location span');
+let conditionText = document.querySelector('.weather_condition span');
+let conditionImage = document.querySelector('.weather_condition img');
 
-let apiUrl = 'http://api.weatherapi.com/v1/current.json?key=882f1adb930a45d7852160114232705&q=New York&aqi=no';
+
 
 // Make a GET request
-fetch(apiUrl)
+
+
+
+// btn.addEventListener('click',function(){
+//     let value = input.value;
+//     console.log(value)
+// })
+
+form.addEventListener('submit',function(e){
+  e.preventDefault(); // it will prevent from page refresh after submitting the form. 
+  let value = input.value;
+  console.log(value)
+  let apiUrl = 'http://api.weatherapi.com/v1/current.json?key=882f1adb930a45d7852160114232705&q='+value+'&aqi=no';
+  fetch(apiUrl)
   .then(response => {
+    // console.log(response);
     if (!response.ok) {
       throw new Error('Network response was not ok');
     }
@@ -17,13 +37,17 @@ fetch(apiUrl)
   })
   .then(data => {
     console.log(data);
+    temp.innerText = data.current.temp_c;
+    place.innerText = data.location.name;
+    timeDate.innerText = data.location.localtime
+    conditionText.innerText = data.current.condition.text;
+    let imgSrc = data.current.condition.icon;
+    // https://cdn.weatherapi.com/weather/64x64/night/113.png
+    // console.log(imgSrc.substring(2));
+    conditionImage.src = "https:"+imgSrc;
   })
   .catch(error => {
+    alert("Sorry, for the incovenienc, please try again with correct name")
     console.error('Error:', error);
   });
-
-
-btn.addEventListener('click',function(){
-    let value = input.value;
-    console.log(value)
 })
