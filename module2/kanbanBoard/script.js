63e6005db001af33cd2f8f8eba6f668bbb594123
aman-scaler <mohd.aman@scaler.com>
2023-12-30 07:52:36 +0530

    filter implemented

--- module2/kanbanBoard/script.js
+++ module2/kanbanBoard/script.js
@@ -27,6 +27,33 @@ if(localStorage.getItem('TaskArr')){
     }
 }
 
+//filter of ticket
+let filterColor = document.querySelectorAll('.color');
+for(let i=0;i<filterColor.length;i++){
+    filterColor[i].addEventListener('click',function(){
+        let allTicketsColor = document.querySelectorAll('.ticket-color');
+        // console.log(filterColor[i]);
+        let filterSelectdColor = filterColor[i].classList[1];
+        for(let j=0;j<allTicketsColor.length;j++){
+            // console.log(allTicketsColor[j]);
+            let currentTicketColor = allTicketsColor[j].classList[1];
+            // console.log(currentTicketColor);
+            if(filterSelectdColor == currentTicketColor){
+                allTicketsColor[j].parentElement.style.display = 'block' // show it
+            }else{
+                allTicketsColor[j].parentElement.style.display = 'none' // hide it
+            }
+        }
+    })
+
+    filterColor[i].addEventListener('dblclick',function(){
+        let allTicketsColor = document.querySelectorAll('.ticket-color');
+        for(let j=0;j<allTicketsColor.length;j++){
+            allTicketsColor[j].parentElement.style.display = 'block' // show it
+        }
+    })
+}
+
 let allPriorityColor = document.querySelectorAll('.priority-color');
 let priorityColor = 'red';
 for(let i=0;i<allPriorityColor.length;i++){
