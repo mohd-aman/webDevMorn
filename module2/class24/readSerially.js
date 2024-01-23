5e804fc30a242e24d1e77d0353f2104c6bb3b656
aman-scaler <mohd.aman@scaler.com>
2024-01-23 09:04:16 +0530

    promise chaining

---
+++ module2/class24/readSerially.js
@@ -0,0 +1,13 @@
+let fs = require('fs');
+
+let f1Promise = fs.promises.readFile('f1.txt');
+
+f1Promise.then(function(data){
+    console.log(data+"");
+    return fs.promises.readFile('f2.txt');
+}).then(function(data){
+    console.log(data+"")
+    return fs.promises.readFile('f3.txt');
+}).then(function(data){
+    console.log(data+"");
+})
