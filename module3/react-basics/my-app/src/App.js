2a7fa411b42dd28f93694eaa04a22b83359b1c06
aman-scaler <mohd.aman@scaler.com>
2024-02-06 09:19:15 +0530

    react intro, setup and installation

---
+++ module3/react-basics/my-app/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="parent">
+      <h1 class="greeting" id="one">Hello </h1>
+      <h2 class="question" id="two">How are you</h2>
+    </div>
+  );
+}
+
+export default App;
