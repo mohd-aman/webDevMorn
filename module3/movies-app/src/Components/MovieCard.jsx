b91052ff8eb9c39c8431ce9913b9b4b968787e70
aman-scaler <mohd.aman@scaler.com>
2024-02-22 09:17:05 +0530

    homepage completed

--- module3/movies-app/src/Components/MovieCard.jsx
+++ module3/movies-app/src/Components/MovieCard.jsx
@@ -1,10 +1,21 @@
 
 
-export default function MovieCard({title, poster_path}){
+export default function MovieCard({title, 
+    id,
+    poster_path,
+    watchlist,
+    handleAddToWatchList,
+    handleRemoveFromWatchList}){
+
     return(
-        <div className="flex justify-center items-end h-80 w-56 rounded-lg overflow-hidden bg-cover bg-no-repeat"
+        <div className="flex flex-col justify-between hover:scale-[0.9] duration-300 justify-center items-end h-80 w-56 rounded-lg overflow-hidden bg-cover bg-no-repeat"
         style={{backgroundImage:`url(https://image.tmdb.org/t/p/original${poster_path})`}}
         >
+            { watchlist.includes(id)?
+            <div onClick={()=>handleRemoveFromWatchList(id)} className="text-xl m-2 rounded-lg cursor-pointer p-2 bg-slate-950/50">&#10060;</div> :
+            <div onClick={()=>handleAddToWatchList(id)} className="text-xl m-2 rounded-lg cursor-pointer p-1 bg-slate-950/50">&#128525;</div>
+            }
+
           <div className='text-white py-0.5 bg-slate-950/50 w-full text-center'>
             {title}
           </div>
--- module3/movies-app/src/Components/TrendingMovies.jsx
+++ module3/movies-app/src/Components/TrendingMovies.jsx
@@ -6,6 +6,21 @@ import axios from 'axios';
 function TrendingMovies() {
   const [movies,setMovies] = useState([]);
   const [pageNo,setPageNo] = useState(1);
+  const [watchlist,setWatchList] = useState([]);
+
+  const handleAddToWatchList = (id)=>{
+    const newWatchList = [...watchlist];
+    newWatchList.push(id);
+    localStorage.setItem('watchList',JSON.stringify(newWatchList));
+    setWatchList(newWatchList);
+  }
+  const handleRemoveFromWatchList = (id)=>{
+    const newWatchList = watchlist.filter((movieId)=>{
+      return id!==movieId;
+    })
+    localStorage.setItem('watchList',JSON.stringify(newWatchList));
+    setWatchList(newWatchList);
+  }
 
   const handleNext = function(){
     setPageNo(pageNo+1);
@@ -15,6 +30,11 @@ function TrendingMovies() {
     if(pageNo>1)
       setPageNo(pageNo-1);
   }
+
+  useEffect(()=>{
+    let watchListFromLocalStorage = JSON.parse(localStorage.getItem('watchList'));
+    setWatchList(watchListFromLocalStorage);
+  },[]) // it will work only on mounting phase
   
   useEffect(()=>{
     axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=52f9641e3d436fb1f176a6290b5a3150&page=${pageNo}`)
@@ -36,8 +56,12 @@ function TrendingMovies() {
           movies.map((movieObj)=>{
             return <MovieCard 
                       key={movieObj.id}
+                      id={movieObj.id}
                       title={movieObj.title} 
                       poster_path={movieObj.poster_path}
+                      watchlist={watchlist}
+                      handleAddToWatchList={handleAddToWatchList}
+                      handleRemoveFromWatchList={handleRemoveFromWatchList}
                    />
           })
         }
