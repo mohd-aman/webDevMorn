4345ed90757a41acd3f53e1ef40af465750dfb29
aman-scaler <mohd.aman@scaler.com>
2024-03-28 09:03:12 +0530

    feat: connect to mongodb and document created

--- module4/class3 and 4/index.js
+++ module4/class3 and 4/index.js
@@ -1,2 +1,39 @@
+const mongoose = require('mongoose');
+
 const password = "xG7PV1E9QM4HUWAT";
 const url = "mongodb+srv://saifiamaan445:xG7PV1E9QM4HUWAT@cluster0.kdkduy1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+mongoose.connect(url)
+.then(()=>{
+    console.log("Connection Established")
+}).catch((error)=>{
+    console.log(error);
+})
+
+//Schema
+
+const courseSchema = new mongoose.Schema({
+    name:String,
+    creator:String,
+    rating:Number,
+    publishedDate:{type:Date,default:Date.now}
+})
+
+//Model
+const Course = mongoose.model('Course',courseSchema);
+//created the collection in mongodb
+// and with the help of model we will be creating document
+
+//CRUD
+
+async function createCourse(){
+    const course = new Course({
+        name:"React",
+        creator:"Aman",
+        rating:5, 
+    })
+    const courseCreated = await course.save();
+    console.log(courseCreated);
+}
+
+createCourse();
